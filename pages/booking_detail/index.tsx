4a2b7ecc8bc230681043b6e4fb508da5d1370d0b
khoi <khoidolce@gmail.com>
2022-10-12 14:05:13 +0700

    DATN29-31 upload modal book

--- pages/booking_detail/index.tsx
+++ pages/booking_detail/index.tsx
@@ -73,7 +73,7 @@ const BookingDetail = (props: Props) => {
                 {/*header*/}
                 <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-3xl font-semibold">
-                    Book
+                    Booking
                   </h3>
                   <button
                     className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"

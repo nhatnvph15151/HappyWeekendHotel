8a65aa5a921f7a6f53f28de8871aede23e68fdcd
= <=>
2022-10-20 22:40:26 +0700

    ckeditor

--- pages/admin/room/add.tsx
+++ pages/admin/room/add.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 import { DashboardLayout } from '../../../components/dashboard-layout'
 import useProducts from '../../../hook/use-product'
+import { CKEditor } from 'ckeditor4-react';
 
 type Props = {}
 
@@ -45,6 +46,10 @@ const AddRoom = (props: Props) => {
       }
     })
   }
+
+  // const getDesc = (e) => {
+  //   var desc = CKEDITOR.instances['DSC'].getData();
+  // }
   return (
     <div className='flex w-[100vh] min-w-full items-center justify-center'>
       <form onSubmit={handleSubmit(themsp)} className='m-4 p-4 shadow-xl rounded w-full'>
@@ -84,11 +89,10 @@ const AddRoom = (props: Props) => {
             </select>
           </div>
         </div>
-        <div className="grid md:grid-cols-2 md:gap-6">
-          <div className="relative z-0 mb-6 w-full group">
-            <input type="text" {...register("description")} name="description" id="description" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-            <label htmlFor="description" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
-          </div>
+        <div className="relative z-0 mb-6 w-full group">
+          <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Mô tả phòng</label>
+          <CKEditor
+            initData="<p>This is an example CKEditor 4 WYSIWYG editor instance.</p>" />
         </div>
         <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
       </form>

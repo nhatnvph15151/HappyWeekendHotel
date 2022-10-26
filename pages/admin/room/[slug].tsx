32282dbb020e5260c84098818415745a68c73718
= <=>
2022-10-26 23:38:07 +0700

    DATN29-76 <fix bug edit room>

--- pages/admin/room/[slug].tsx
+++ pages/admin/room/[slug].tsx
@@ -8,6 +8,8 @@ import useProducts from '../../../hook/use-product';
 import useBasic from '../../../hook/use-basic'
 import { useRouter } from 'next/router'
 import CKeditor from '../../../components/CkEditor'
+import App from '../../../components/CkEditor'
+import e from 'express'
 
 type Props = {}
 
@@ -32,33 +34,53 @@ const Detail = (props: Props) => {
 
   const { register, handleSubmit, formState: { errors }, reset } = useForm();
 
+  function parentCallBack(child: any) {
+    setdesc(child)
+  }
+
+  function getDesc() {
+    return room.data?.description
+  }
+
   React.useEffect(() => {
     reset(room.data)
     setEditorLoaded(true)
+    setdesc(room.data?.description)
   }, [room.data])
 
   const themsp: SubmitHandler<any> = (data: any) => {
     setLoading(false)
     const file = data.image[0]
-    const formData = new FormData()
-
-    formData.append('file', file)
-    formData.append("upload_preset", "hzeskmhn")
-
-    axios({
-      url: 'https://api.cloudinary.com/v1_1/dkhutgvlb/image/upload',
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-formendcoded",
-      }, data: formData,
-    }).then((res) => {
-      data.image = res.data.url
+    data.description = desc
+    if (file !== 'h') {
+      const formData = new FormData()
+      formData.append('file', file)
+      formData.append("upload_preset", "hzeskmhn")
+      axios({
+        url: 'https://api.cloudinary.com/v1_1/dkhutgvlb/image/upload',
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-formendcoded",
+        }, data: formData,
+      })
+        .then((res) => {
+          data.image = res.data.url
+          data.description = desc
+          try {
+            room.edit(data).then(() => {
+              setLoading(true)
+            })
+          } catch (error) { }
+        })
+    }
+    else {
+      data.description = desc
       try {
         room.edit(data).then(() => {
           setLoading(true)
         })
       } catch (error) { }
-    })
+    }
   }
 
   return (
@@ -72,15 +94,13 @@ const Detail = (props: Props) => {
             <input type="text" {...register("name")} name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
             <label htmlFor="name" className="z-50 peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Tên phòng</label>
           </div>
-          <div className="grid md:grid-cols-2 md:gap-6">
-            <div className="relative z-0 mb-6 w-full group">
-              <input type="number" {...register("price")} name="price" id="price" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-              <label htmlFor="price" className="z-50 peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Giá phòng</label>
-            </div>
-            <div className="relative z-0 mb-6 w-full group">
-              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" htmlFor="file_input">Ảnh phòng</label>
-              <input name="image" {...register('image')} className="block w-full text-sm text-gray-900 bg-gray-50 cursor-pointer dark:text-gray-400 focus:outline-none" id="file_input" type="file" />
-            </div>
+          <div className="relative z-0 mb-6 w-full group">
+            <input type="number" {...register("price")} name="price" id="price" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+            <label htmlFor="price" className="z-50 peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Giá phòng</label>
+          </div>
+          <div className="relative z-0 mb-6 w-full group">
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" htmlFor="file_input">Ảnh phòng</label>
+            <input name="image" {...register('image')} className="block w-full text-sm text-gray-900 bg-gray-50 cursor-pointer dark:text-gray-400 focus:outline-none" id="file_input" type="file" />
           </div>
           <div className="grid md:grid-cols-2 md:gap-6">
             <div className="relative z-0 mb-6 w-full group">
@@ -107,13 +127,9 @@ const Detail = (props: Props) => {
 
           <div className="relative z-0 mb-6 w-full group">
             <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Mô tả phòng</label>
-            <CKeditor
-              name="description"
-              value={room.data?.description}
-              onChange={(data: any) => {
-                setdesc(data);
-              }}
-              editorLoaded={editorLoaded}
+            <App
+              parentCallBack={parentCallBack}
+              getDesc={getDesc}
             />
           </div>
           <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>

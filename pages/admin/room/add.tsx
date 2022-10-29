205a23dbb634ed19e468490549c5f8dac398154b
nhatnvph15151 <nnhat1802@gmail.com>
2022-10-29 15:35:30 +0700

    validate room admin

--- pages/admin/room/add.tsx
+++ pages/admin/room/add.tsx
@@ -84,16 +84,36 @@ const AddRoom = (props: Props) => {
       <div className='flex w-[100vh] min-w-full items-center justify-center'>
         <form onSubmit={handleSubmit(themsp)} className='m-4 p-4 shadow-xl rounded w-full'>
           <div className="relative z-0 mb-6 w-full group">
-            <input type="text" {...register("name")} name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+            <input type="text" {...register("name",{required:true ,minLength:{value:1,message:"khong duoc de trong"}})}  name="name" id="name" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
             <label htmlFor="name" className="z-50 peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Tên phòng</label>
+            {Object.keys(errors).length !== 0 && (
+              <ul>
+                {errors.name?.type == 'required' && (<li className='text-[red]'>Không được trống</li>)}
+                {errors.name?.message && <p className='text-[red]'>{errors.name.message}</p>}
+              </ul>
+            )}
           </div>
+
+
           <div className="relative z-0 mb-6 w-full group">
-            <input type="number" {...register("price")} name="price" id="price" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+            <input type="number" {...register("price", { required: true, minLength: { value: 1, message: "khong duoc de trong" } })} name="price" id="price" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
             <label htmlFor="price" className="z-50 peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Giá phòng</label>
+            {Object.keys(errors).length !== 0 && (
+              <ul>
+                {errors.price?.type == 'required' && (<li className='text-[red]'>Không được trống</li>)}
+                {errors.price?.message && <p className='text-[red]'>{errors.price.message}</p>}
+              </ul>
+            )}
           </div>
           <div className="relative z-0 mb-6 w-full group">
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" htmlFor="file_input">Ảnh phòng</label>
-            <input name="image" {...register('image')} className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" />
+            <input name="image" {...register('image', { required: true, minLength: { value: 1, message: "khong duoc de trong" } })} className="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" />
+            {Object.keys(errors).length !== 0 && (
+              <ul>
+                {errors.image?.type == 'required' && (<li className='text-[red]'>Không được trống</li>)}
+                {errors.image?.message && <p className='text-[red]'>{errors.image.message}</p>}
+              </ul>
+            )}
           </div>
           <div className="relative z-0 mb-6 w-full group">
             <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Chọn một loại phòng</label>

60a416443f14cead9475c1701ae80fca2877b0be
Nguyễn Quý Giang <giangnq@its-global.vn>
2022-12-06 17:16:03 +0700

    feat: ui Price Booking Detail

--- pages/admin/order/[id].tsx
+++ pages/admin/order/[id].tsx
@@ -59,13 +59,13 @@ const DetailOrder = (props: Props) => {
                 <option value="4" >Hủy</option>
             </select>
         } else if (status?.statusorder == 1) {
-            return <select id="" {...register('statusorder')}>
+            return <select id="" {...register('statusorder')} className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
                 <option value="2">Đang có khách</option>
                 <option value="3">Đã trả phòng</option>
                 <option value="4">Hủy</option>
             </select>
         } else if (status?.statusorder == 2) {
-            return <select id="" {...register('statusorder')}>
+            return <select id="" {...register('statusorder')} className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
                 <option value="3">Đã trả phòng</option>
                 {/* <option value="4">Hủy</option> */}
             </select>
@@ -137,7 +137,7 @@ const DetailOrder = (props: Props) => {
                                 <div className='grid grid-cols-2 gap-4 mt-[10px]'>
                                     {facilities?.map((item: any) => (
                                         <div className='flex '>
-                                            <img width={25} src={item.image} alt="" />
+                                            <img width={30} src={item.image} alt="" />
                                             <p className=' ml-[5px] self-center'>{item.name}</p>
                                         </div>
                                     ))}

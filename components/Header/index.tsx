513a64e83df74ea0df7e4f9a63a5651ee772dcf8
Nguyễn Trần Khôi <khoidolce@gmail.com>
2022-12-30 20:42:39 +0700

    DATN29-103 update search room header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,19 +1,53 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from 'next/link'
-import DatePicker from "../../components/DatePicker"
-import React, { useEffect, useState } from 'react'
+import React, { ChangeEvent, FormEvent, MouseEvent, useEffect, useMemo, useState } from 'react'
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import { json } from 'node:stream/consumers';
+import { DateRangePicker, DateRangeDelimiter, LocalizationProvider, DateTimePicker } from "@material-ui/pickers";
+import { InputAdornment } from "@mui/material";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
 import { useRouter } from 'next/router';
 import { USER_ROLE } from '../../constants';
+import dayjs, { Dayjs } from 'dayjs';
+import TextField from "@material-ui/core/TextField";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import LogoutIcon from "@mui/icons-material/Logout";
+import DateFnsUtils from "@material-ui/pickers/adapter/date-fns";
+import LoginIcon from "@mui/icons-material/Login";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import toastr from "toastr";
+import "toastr/build/toastr.min.css";
+
 type Props = {}
 
 const Header = (props: Props) => {
-  const [anchorEl2, setAnchorEl2] = React.useState<null | HTMLElement>(null);
+  const defaultSelectedDate = useMemo(() => {
+    const currentDate = new Date();
+    const futureDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 1);
+
+    return [dayjs(currentDate.toISOString()), dayjs(futureDate.toISOString())];
+  }, []);
+
+  const [selectedDate, setSelectedDate] = useState<any>(defaultSelectedDate);
+
+  // thời gian nhận phòng - form tìm kiếm theo giờ
+  const [dateTimeStart, setDateTimeStart] = useState<Dayjs | null>(() => {
+    const currentDate = new Date();
+    const futureDate = new Date(currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() + 1, 6, 30);
+
+    return dayjs(futureDate.toISOString());
+  });
+
+  // thời gian trả phòng - form tìm kiếm theo giờ
+  const [hours, setHours] = useState<number>(2);
+
+  const [anchorEl2, setAnchorEl2] = useState<null | HTMLElement>(null);
   const open2 = Boolean(anchorEl2);
-  const handleClick2 = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleClick2 = (event: MouseEvent<HTMLButtonElement>) => {
     setAnchorEl2(event.currentTarget);
   };
   const handleClose2 = () => {
@@ -62,13 +96,163 @@ const Header = (props: Props) => {
     }
   }, [query])
 
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const open1 = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
+  };
+
+  const DateRangerPicker = () => {
+    return (
+      <LocalizationProvider dateAdapter={DateFnsUtils as any}>
+        <DateRangePicker
+          startText="Nhận phòng"
+          inputFormat="dd/MM/YYY"
+          endText="Trả phòng"
+          value={selectedDate}
+          disablePast
+          onChange={(date: any) => setSelectedDate(date)}
+          renderInput={(startProps, endProps) => (
+            <>
+              <TextField
+                {...startProps}
+                size="small"
+                variant="standard"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LoginIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                helperText=""
+              />
+              <DateRangeDelimiter> to </DateRangeDelimiter>
+              <TextField
+                {...endProps}
+                size="small"
+                variant="standard"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LogoutIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                error={!selectedDate[1]}
+                helperText=""
+              />
+            </>
+          )}
+        />
+      </LocalizationProvider>
+    );
+  };
+
+  const DateTimePickers = () => {
+    return (
+      <>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DateTimePicker
+            disablePast
+            label="Nhận phòng"
+            inputFormat="HH:mm, DD [tháng] MM"
+            renderInput={(params) => <TextField {...params} size="small" helperText="" />}
+            value={dateTimeStart}
+            onChange={(newValue) => {
+              setDateTimeStart(newValue);
+            }}
+          />
+        </LocalizationProvider>
+
+        <div className="ml-2 mr-3 min-w-[150px]">
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Số giờ sử dụng</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={hours as any}
+              label="Age"
+              onChange={(e: ChangeEvent<any>) => {
+                setHours(+e.target.value);
+              }}
+            >
+              <MenuItem value={1}>1 giờ</MenuItem>
+              <MenuItem value={2}>2 giờ</MenuItem>
+              <MenuItem value={3}>3 giờ</MenuItem>
+              <MenuItem value={4}>4 giờ</MenuItem>
+              <MenuItem value={5}>5 giờ</MenuItem>
+              <MenuItem value={6}>6 giờ</MenuItem>
+              <MenuItem value={7}>7 giờ</MenuItem>
+              <MenuItem value={8}>8 giờ</MenuItem>
+              <MenuItem value={9}>9 giờ</MenuItem>
+              <MenuItem value={10}>10 giờ</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+      </>
+    );
+  };
+
+  // search room.
+  const handleSearch = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!selectedDate[0] || !selectedDate[1]) {
+      toastr.info("Vui lòng chọn thời gian trả phòng!");
+      return;
+    }
+
+    let query = {};
+
+    // tìm kiếm theo giờ.
+    if (indexTab === 1) {
+      const timeCheckin = new Date(dateTimeStart as any);
+
+      query = {
+        checkin: new Date(
+          timeCheckin.getFullYear(),
+          timeCheckin.getMonth(),
+          timeCheckin.getDate(),
+          timeCheckin.getHours(),
+          timeCheckin.getMinutes(),
+          timeCheckin.getSeconds(),
+        ).toISOString(),
+        checkout: new Date(
+          timeCheckin.getFullYear(),
+          timeCheckin.getMonth(),
+          timeCheckin.getDate(),
+          timeCheckin.getHours() + hours,
+          timeCheckin.getMinutes(),
+          timeCheckin.getSeconds(),
+        ).toISOString(),
+      };
+    } else {
+      const [checkin, checkout] = selectedDate;
+      const dateCheckin = new Date(checkin);
+      const dateCheckout = new Date(checkout);
+
+      // tìm kiếm phòng qua đêm, theo ngày mặc định thời gian checkin là 14h và checkout là 12h trưa hôm sau.
+      query = {
+        checkin: new Date(dateCheckin.getFullYear(), dateCheckin.getMonth(), dateCheckin.getDate(), 14).toISOString(),
+        checkout: new Date(
+          dateCheckout.getFullYear(),
+          dateCheckout.getMonth(),
+          dateCheckout.getDate(),
+          12,
+        ).toISOString(),
+      };
+    }
+
+    setOpen(true);
+
+    router.push({
+      pathname: "search",
+      query,
+    });
   };
 
   return (
@@ -122,7 +306,7 @@ const Header = (props: Props) => {
                     <input
                       id='inputStartDate'
                       readOnly
-                      value="17/10/2022"
+                      value={indexTab !== 1 ? dayjs(selectedDate[0]).format("DD/MM/YYYY") : dayjs(dateTimeStart).format("DD/MM/YYYY")}
                       className="p-2 outline-none font-semibold ml-4"
                     />
                   </div>
@@ -134,7 +318,7 @@ const Header = (props: Props) => {
                     </label>
                     <input
                       id='inputEndDate'
-                      value="17/11/2022"
+                      value={indexTab !== 1 ? dayjs(selectedDate[1]).format("DD/MM/YYYY") : dayjs(dateTimeStart).add(hours, "hour").format("DD/MM/YYYY")}
                       readOnly
                       className="p-2 outline-none font-semibold ml-4"
                     />
@@ -150,23 +334,31 @@ const Header = (props: Props) => {
               </div>
             </div>
           }
-          <div className={`${!open ? "visible scale-100 opacity-100" : "invisible scale-50 opacity-0"} duration-300 translate-x-[-50%] absolute top-[74px] left-[50%] w-full mx-auto bg-white shadow-xl p-4`}>
+          <form onSubmit={handleSearch} className={`${!open ? "visible scale-100 opacity-100" : "invisible scale-50 opacity-0"} duration-300 translate-x-[-50%] absolute top-[74px] left-[50%] w-full mx-auto bg-white shadow-xl p-4`}>
             <div className="flex justify-center">
-              <button className={`${indexTab == 1 ? 'text-[red] border-b border-[red]' : null} duration-150 hover:text-[red] flex flex-col items-center px-4`} onClick={() => { setIndexTab(1) }}>
+              <button type='button' className={`${indexTab == 1 ? 'text-[red] border-b border-[red]' : null} duration-150 hover:text-[red] flex flex-col items-center px-4`} onClick={() => { setIndexTab(1) }}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
                 <p>Theo giờ</p>
               </button>
 
-              <button className={`${indexTab == 2 ? 'text-[red] border-b border-[red]' : null} duration-150 hover:text-[red] flex flex-col items-center px-4`} onClick={() => { setIndexTab(2) }}>
+              <button type='button' className={`${indexTab == 2 ? 'text-[red] border-b border-[red]' : null} duration-150 hover:text-[red] flex flex-col items-center px-4`} onClick={() => {
+                setIndexTab(2);
+                setSelectedDate(defaultSelectedDate);
+                }}
+              >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
                 </svg>
                 <p>Qua đêm</p>
               </button>
 
-              <button className={`${indexTab == 3 ? 'text-[red] border-b border-[red]' : null} duration-150 hover:text-[red] flex flex-col items-center px-4`} onClick={() => { setIndexTab(3) }}>
+              <button type='button' className={`${indexTab == 3 ? 'text-[red] border-b border-[red]' : null} duration-150 hover:text-[red] flex flex-col items-center px-4`} onClick={() => {
+                setIndexTab(3);
+                setSelectedDate(defaultSelectedDate);
+                }}
+              >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5m-9-6h.008v.008H12v-.008zM12 15h.008v.008H12V15zm0 2.25h.008v.008H12v-.008zM9.75 15h.008v.008H9.75V15zm0 2.25h.008v.008H9.75v-.008zM7.5 15h.008v.008H7.5V15zm0 2.25h.008v.008H7.5v-.008zm6.75-4.5h.008v.008h-.008v-.008zm0 2.25h.008v.008h-.008V15zm0 2.25h.008v.008h-.008v-.008zm2.25-4.5h.008v.008H16.5v-.008zm0 2.25h.008v.008H16.5V15z" />
                 </svg>
@@ -174,9 +366,9 @@ const Header = (props: Props) => {
               </button>
             </div>
             <div className={`border rounded-full p-4 mt-4 relative flex justify-center`}>
-              {indexTab == 1 ? <DatePicker date={7} /> : ""}
-              {indexTab == 2 ? <DatePicker date={1} /> : ""}
-              {indexTab == 3 ? <DatePicker date={28} /> : ""}
+              {indexTab == 1 && <DateTimePickers />}
+              {indexTab == 2 && <DateRangerPicker />}
+              {indexTab == 3 && <DateRangerPicker />}
               {/* <div className="relative top-[50%] translate-y-[30%]">
                     <div className="flex">
                       <div className="relative">
@@ -208,14 +400,12 @@ const Header = (props: Props) => {
                       </div>
                     </div>
                   </div> */}
-              <div className="flex px-4 py-2 bg-[orange] rounded-full text-white cursor-pointer">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                </svg>
-                <p>Tìm kiếm</p>
-              </div>
+              <button type='submit' className="flex px-4 py-2 items-center bg-[orange] rounded-full text-white cursor-pointer">
+                <FontAwesomeIcon icon={faSearch} />
+                <p className='ml-2'>Tìm kiếm</p>
+              </button>
             </div>
-          </div>
+          </form>
           <div className="flex items-center">
             <Link href="/">
               <span className='px-4 py-2 border border-black rounded-full cursor-pointer hover:opacity-50 duration-150'>Dành cho đối tác</span>

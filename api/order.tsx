196565847f6d206d478a43c8d1c172ad2b84583c
Nguyễn Trần Khôi <khoidolce@gmail.com>
2023-01-09 14:02:46 +0700

    DATN29-123 format code

--- api/order.tsx
+++ api/order.tsx
@@ -1,35 +1,34 @@
 import { OrderType } from "../types/order";
 import instance from "./instance";
 
-
-export const creatOrder = (item:OrderType)=>{
-    const url = 'order'
-    return instance.post(url,item)
-}
-export const sendMail = (item:any)=>{
-    const url = 'sendMail'
-    return instance.post(url,item)
-}
+export const creatOrder = (item: OrderType) => {
+  const url = "order";
+  return instance.post(url, item);
+};
+export const sendMail = (item: any) => {
+  const url = "sendMail";
+  return instance.post(url, item);
+};
 
 export const listOrder = () => {
-    const url = 'order'
-    return instance.get(url)
-}
+  const url = "order";
+  return instance.get(url);
+};
 
-export const detail = (id:any|undefined) => {
-    const url = `order/${id}`
-    return instance.get(url)
-}
-export const listOrderUser = (id:any) => {
-    const url = `orders/${id}`
-    return instance.get(url)
-}
-export const update = (item:OrderType) => {
-    const url = `order/${item._id}/edit`
-    return instance.put(url, item)
-}
+export const detail = (id: any | undefined) => {
+  const url = `order/${id}`;
+  return instance.get(url);
+};
+export const listOrderUser = (id: any) => {
+  const url = `orders/${id}`;
+  return instance.get(url);
+};
+export const update = (item: OrderType) => {
+  const url = `order/${item._id}/edit`;
+  return instance.put(url, item);
+};
 
-export const checkUserBookRoom = (data: { user: string; room?: string }): Promise<{ isBooked: boolean }> => {
-    const url = "order/checkUserBookRoom";
-    return instance.post(url, data);
-}
+export const checkUserBookRoom = (data: { user: string; room?: string }): Promise<{ isBooked: boolean  }> => {
+  const url = "order/checkUserBookRoom";
+  return instance.post(url, data);
+};
--- components/Dialog/index.tsx
+++ components/Dialog/index.tsx
@@ -1,37 +1,32 @@
+import CloseIcon from "@mui/icons-material/Close";
+import { Dialog, DialogActions, DialogTitle, IconButton } from "@mui/material";
 import React, { forwardRef, useImperativeHandle, useState } from "react";
-import { creatOrder, sendMail } from "../../api/order";
-import Swal from 'sweetalert2'
+import { creatOrder } from "../../api/order";
+import Swal from "sweetalert2";
+import { update } from "../../api/voucher";
 import { creat } from "../../api/bookedDate";
 import { useRouter } from "next/router";
-import { Button, Dialog, DialogActions, DialogTitle, IconButton } from "@mui/material";
-import CloseIcon from '@mui/icons-material/Close';
 import { bangking } from "../../api/banking";
-import { update } from "../../api/voucher";
 import dayjs from "dayjs";
-
-type PostProps = {
-
-};
 
 const DialogConfirm = ({ data, datebooks, room }: any, ref: any) => {
   const [displayBasic2, setDisplayBasic2] = useState<any>(false);
-  const router = useRouter()
-  
+  const router = useRouter();
+
   const handleClose = () => {
-    setDisplayBasic2(false)
-  }
+    setDisplayBasic2(false);
+  };
 
   useImperativeHandle(ref, () => ({
     open: (props: any) => {
-      setDisplayBasic2(true)
-    }
-  }))
- 
+      setDisplayBasic2(true);
+    },
+  }));
+
   const order = async () => {
-    console.log(data);
     await creat(datebooks)
-      .then(async (res: any) => {
-        const newdata: any = {
+      .then(async(res: any) => {
+        const newdataOrder: any = {
           status: res._id,
           name: data.name,
           email: data.email,
@@ -41,59 +36,49 @@ const DialogConfirm = ({ data, datebooks, room }: any, ref: any) => {
           checkouts: data.checkouts,
           room: data.room,
           statusorder: data.statusorder,
-          user: data.user,
-        }
-
-        if (data.voucher) {
-          newdata.voucher = data.voucher._id;
-        }
-
-        console.log(res._id)
-        const disabledDateBooked = async () => {
-          await creatOrder(newdata)
-        }
-        await disabledDateBooked();
-     
-        // giảm số lượng voucher, lưu id user sử dụng voucher.
-        if (data.user && data.voucher) {
-          const users = [...data.voucher.users, data.user];
-          await update({
-            ...data.voucher,
-            quantity: data.voucher.quantity - 1 >= 0 ? data.voucher.quantity - 1 : 0,
-            users
-          })
-        }
-        Swal.fire(
-          'Đặt phòng thành công',
-          'Thông tin chi tiết sẽ được gửi tới email của bạn.',
-          'success'
-        )
-        handleClose();
+          user: data.user
+        };
+        // check nếu user áp voucher
+        if (data && data.voucher) {
+          newdataOrder.voucher = data.voucher._id;
+        };
+        await creatOrder(newdataOrder);
+        // giảm số lượng voucher, lưu id user nếu user sử dụng voucher.
+        if (data.voucher && data.user) {
+          const users = [ ...data.voucher.users, data.user ];
+          const voucherData = {
+              ...data.voucher,
+              quantity: data.voucher.quantity - 1 >= 0 ? data.voucher.quantity - 1 : 0,
+              users,
+          }
+          await update(voucherData);
+        };
+        Swal.fire("Đặt phòng thành công", "Thông tin chi tiết sẽ được gửi tới email của bạn.", "success");
+        handleClose()
       })
       .catch(() => {
         Swal.fire({
-          title: 'Có lỗi gì đó.',
-          icon: 'error'
-        })
-      })
+          title: "Có lỗi gì đó.",
+          icon: "error",
+        });
+      });
   }
-
-  // format tiền.
-  const formatCurrency = (currency: number) => {
-    const tempCurrency = +currency >= 0 ? currency : 0;
-    return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'VND' }).format(tempCurrency)
-  };
-
+  //format tiền.
+  const formatCurrency = (currency: any) => {
+    const money = +currency >= 0 ? currency : 0;
+    return new Intl.NumberFormat("de-DE", { style: "currency", currency: "VND" }).format(money);
+  }
   return (
     <div>
-      <Dialog
-        fullWidth={true}
-        open={displayBasic2}
-        onClose={handleClose}
-      >
+      <Dialog fullWidth={true} open={displayBasic2} onClose={handleClose}>
         <DialogTitle sx={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
           <h1>Kiểm tra thông tin đặt phòng</h1>
-          <IconButton onClick={() => { handleClose() }} aria-label="delete">
+          <IconButton
+            onClick={() => {
+              handleClose();
+            }}
+            aria-label="delete"
+          >
             <CloseIcon />
           </IconButton>
         </DialogTitle>
@@ -102,75 +87,125 @@ const DialogConfirm = ({ data, datebooks, room }: any, ref: any) => {
             <table className=" py-[30px] w-[500px] m-auto drop-shadow-md rounded-md">
               <tbody>
                 <tr className="bg-[#F5FAFF] rounded-lg">
-                  <td className="py-[10px] pl-[20px]"><label htmlFor="" className="font-medium text-[#A7A7A7]">Tên khách hàng</label> </td>
+                  <td className="py-[10px] pl-[20px]">
+                    <label htmlFor="" className="font-medium text-[#A7A7A7]">
+                      Tên khách hàng
+                    </label>{" "}
+                  </td>
                   <td className="pl-[42px] text-[#424241]">{data.name}</td>
                 </tr>
                 <tr className="bg-[#ffffff]">
-                  <td className="py-[10px] pl-[20px]"><label htmlFor="" className="font-medium text-[#A7A7A7]">Email </label> </td>
+                  <td className="py-[10px] pl-[20px]">
+                    <label htmlFor="" className="font-medium text-[#A7A7A7]">
+                      Email{" "}
+                    </label>{" "}
+                  </td>
                   <td className="pl-[42px] text-[#424241]"> {data.email}</td>
                 </tr>
                 <tr className="bg-[#F5FAFF] ">
-                  <td className="py-[10px] pl-[20px]"><label htmlFor="" className="font-medium text-[#A7A7A7]">Số điện thoại</label> </td>
+                  <td className="py-[10px] pl-[20px]">
+                    <label htmlFor="" className="font-medium text-[#A7A7A7]">
+                      Số điện thoại
+                    </label>{" "}
+                  </td>
                   <td className="py-[5px] pl-[42px] text-[#424241]">{data.phone}</td>
                 </tr>
                 <tr className=" bg-[#ffffff]">
-                  <td className="py-[10px] pl-[20px]"><label htmlFor="" className="font-medium text-[#A7A7A7]">Tên phòng đặt</label> </td>
+                  <td className="py-[10px] pl-[20px]">
+                    <label htmlFor="" className="font-medium text-[#A7A7A7]">
+                      Tên phòng đặt
+                    </label>{" "}
+                  </td>
                   <td className="py-[5px] pl-[42px] text-[#424241]">{room}</td>
                 </tr>
-                <tr className="bg-[#F5FAFF]">
-                  <td className="py-[10px] pl-[20px]"><label htmlFor="" className="font-medium text-[#A7A7A7]">Tạm tính</label> </td>
-                  <td className="py-[5px] pl-[42px] text-[#424241]">{formatCurrency(data.total)}</td>
-                </tr>
-                {data.voucher && (
-                  <tr className="bg-[#F5FAFF]">
-                    <td className="py-[10px] pl-[20px]"><label htmlFor="" className="font-medium text-[#A7A7A7]">Voucher</label> </td>
-                    <td className="py-[5px] pl-[42px] text-[#424241]">{data.voucher.code} (-{formatCurrency(data.voucher.discount)})</td>
-                  </tr>
-                )}
+                <tr className="bg-[#f5faff]">
+                  <td className="py-[10px] pl-[20px]">
+                    <label htmlFor="" className="font-medium text-[#A7A7A7]">
+                      Tạm tính
+                    </label>{" "}
+                  </td>
+                  <td className="py-[5px] pl-[42px]  text-[#424241]">{formatCurrency(data.total)}</td>
+                </tr >
+                {data && data.voucher && (
+                  <tr className="bg-[#f5faff]">
+                    <td className="py-[10px] pl-[20px]">
+                      <label htmlFor="" className="font-medium text-[#A7A7A7]">
+                        Voucher
+                      </label>{" "}
+                    </td>
+                    <td className="py-[5px] pl-[42px] text-[#424241]">
+                      {data.voucher.code} (-{formatCurrency(data.voucher.discount)})
+                    </td>
+                  </tr >
+                 )}
                 <tr className=" bg-[#ffffff]">
-                  <td className="py-[10px] pl-[20px]"><label htmlFor="" className="font-medium text-[#A7A7A7]">Tổng tiền</label> </td>
-                  <td className="py-[5px] pl-[42px] text-[#424241]">{formatCurrency(data.total - (data.voucher?.discount || 0))}</td>
+                  <td className="py-[10px] pl-[20px]">
+                    <label htmlFor="" className="font-medium text-[#A7A7A7]">
+                      Tổng tiền
+                    </label>{" "}
+                  </td>
+                  <td className="py-[5px] pl-[42px] text-[#424241]">
+                    {formatCurrency(data.total - (data.voucher?.discount || 0))}
+                  </td>
                 </tr>
                 <tr className="bg-[#F5FAFF] ">
-                  <td className="py-[10px] pl-[20px]"><label htmlFor="" className="font-medium text-[#A7A7A7]">Check In</label> </td>
-                  <td className="py-[5px] pl-[42px] text-[#424241]"> {dayjs(datebooks.dateFrom).format("HH:mm DD/MM/YYYY")} </td>
+                  <td className="py-[10px] pl-[20px]">
+                    <label htmlFor="" className="font-medium text-[#A7A7A7]">
+                      Check In
+                    </label>{" "}
+                  </td>
+                  <td className="py-[5px] pl-[42px] text-[#424241]">
+                    {" "}
+                    {dayjs(datebooks.dateFrom).format("HH:mm DD/MM/YYYY")}{" "}
+                  </td>
                 </tr>
                 <tr className="bg-[#ffffff]">
-                  <td className="py-[10px] pl-[20px]"><label htmlFor="" className="font-medium text-[#A7A7A7]">Check Out</label> </td>
-                  <td className="py-[5px] pl-[42px] text-[#424241]">{dayjs(datebooks.dateTo).format("HH:mm DD/MM/YYYY")}</td>
+                  <td className="py-[10px] pl-[20px]">
+                    <label htmlFor="" className="font-medium text-[#A7A7A7]">
+                      Check Out
+                    </label>{" "}
+                  </td>
+                  <td className="py-[5px] pl-[42px] text-[#424241]">
+                    {dayjs(datebooks.dateTo).format("HH:mm DD/MM/YYYY")}
+                  </td>
                 </tr>
               </tbody>
             </table>
             <div className="max-w-[500px] m-auto pt-[30px]">
               <p className="text-center text-gray-700 text-[15px]">
-                Bạn muốn <span className="font-medium text-green-500 underline decoration-solid">thanh toán trực tiếp</span> hay <span className="font-medium text-blue-500 underline decoration-solid">thanh toàn trực tuyến</span> thì vui lòng chọn vào mục bạn mốn!
+                Bạn muốn{" "}
+                <span className="font-medium text-green-500 underline decoration-solid">thanh toán trực tiếp</span> hay{" "}
+                <span className="font-medium text-blue-500 underline decoration-solid">thanh toàn trực tuyến</span> thì
+                vui lòng chọn vào mục bạn mốn!
               </p>
             </div>
-
           </div>
           <DialogActions sx={{ display: "flex", justifyContent: "center" }}>
             <div className="flex">
               <div className="mr-[20px]">
-                <button
-                  className="px-4 py-2 rounded-md shadow-xl bg-[green] text-white"
-                  onClick={() => order()}
-                >Thanh toán trực tiếp</button>
+                <button className="px-4 py-2 rounded-md shadow-xl bg-[green] text-white" onClick={() => order()}>
+                  Thanh toán trực tiếp
+                </button>
               </div>
               <div>
                 <button
                   className="px-4 py-2 rounded-md shadow-xl bg-[orange] text-white"
                   onClick={() => {
                     // router.push('/payment')
-                    order()
+                    order();
                     bangking({
-                      "total": data.total,
-                      "orderDescription": "",
-                      "orderType": "billpayment",
-                      "language": "vn",
-                      "bankCode": ""
-                    }).then((res: any) => { router.push(`${res.redirect}`) })
+                      total: data.total,
+                      orderDescription: "",
+                      orderType: "billpayment",
+                      language: "vn",
+                      bankCode: "",
+                    }).then((res: any) => {
+                      router.push(`${res.redirect}`);
+                    });
                   }}
-                >Thanh toán trực tuyến</button>
+                >
+                  Thanh toán trực tuyến
+                </button>
               </div>
             </div>
           </DialogActions>
@@ -180,11 +215,3 @@ const DialogConfirm = ({ data, datebooks, room }: any, ref: any) => {
   );
 };
 export default forwardRef(DialogConfirm);
-function handleClose() {
-  throw new Error("Function not implemented.");
-}
-
-function onHide(arg0: string) {
-  throw new Error("Function not implemented.");
-}
-
